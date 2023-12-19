15bf541313e4a6c6eda21064cc265152950ef119
Victoria <sweet.victoria1218@gmail.com>
2023-12-19 12:28:35 -0600

    created schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,15 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE category [if not exists] 'category' (id INT PRIMARY KEY, name VARCHAR(20));
+INSERT INTO category VALUES (varchar);
+INSERT INTO category VALUES ();
+-- -- Category
+-- id
+-- Integer
+-- Doesn't allow null values
+-- Set as primary key
+-- Uses auto increment
+-- category_name
+-- String
+-- Doesn't allow null values
